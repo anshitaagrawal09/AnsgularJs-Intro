76b622178b748e1c7e1f02559c34774e6e057079
anshitaagrawal09 <anshitaagrawal75@gmail.com>
2025-03-22 15:05:35 +0530

    Added functionality to launch BridgeLabz site on clicking BridgeLabz Logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,9 @@ export class AppComponent {
   // title = 'Helloworld';
   title: string = "Hello from BridgeLabz";
   logoUrl: string = "assets/BridgeLabzLogo.png";  // Image path
+  bridgeLabzUrl: string = "https://www.bridgelabz.com"; // Website URL
+
+  openBridgeLabzSite(): void {
+    window.open(this.bridgeLabzUrl, "_blank");  // Open in new tab
+  } 
 }
